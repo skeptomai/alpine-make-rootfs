23c5580f6adebc8d1fc6d5202a8041626453ff1e
Christopher Brown <cb@skeptomai.com>
2022-07-08 13:45:36 -0700

    cleanup and build tini into rootfs

--- drive-make-rootfs.sh
+++ drive-make-rootfs.sh
@@ -1,16 +1,12 @@
 set -xe
 sudo ./alpine-make-rootfs \
     --branch v3.8 \
-    --packages 'ruby ruby-bigdecimal sqlite ca-certificates openssl curl' \
-    --timezone 'Europe/Prague' \
+    --packages 'ca-certificates openssl curl' \
+    --timezone 'America/Los_Angeles' \
     --script-chroot \
     example-$(date +%Y%m%d).tar.gz - <<'SHELL'
-        # Copy some file from the repository root to the rootfs.
-        install -D -m 755 examples/hello_world.rb /app/hello_world.rb
-
-        # Install some dev packages and gem mailcatcher.
-        apk add --no-progress -t .make build-base ruby-dev sqlite-dev
-        gem install --no-document mailcatcher
+        # Install some dev packages.
+        apk add --no-progress --no-cache -t .make
 
         # Clean-up dev packages.  <7>
         apk del --no-progress .make
